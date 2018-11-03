ef6f1e44a43165bc2a38c5cbccdf9752ea94ad16
Erika Kino <o.erika.o@gmail.com>
2018-11-03 02:32:43 -0300

    Change font family

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
 
 configure :development do
   use BetterErrors::Middleware
